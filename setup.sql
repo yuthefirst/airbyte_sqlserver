4020ab926e3dbfa51d0eafc76d186e2c3c495f1a
yuthefirst <khanhduy9cnct@Gmail.com>
2022-08-18 16:41:19 +0700

    Init commit

--- setup.sql
+++ setup.sql
@@ -1,43 +1,43 @@
-CREATE DATABASE Demo_DATABASE;
-GO
-
-USE Demo_DATABASE;
-GO
-
-CREATE TABLE Inventory
-(
-    ID          INT IDENTITY (1, 1),
-    ProductName nvarchar(max),
-    Price       numeric(18, 0)
-);
-GO
-
-INSERT INTO Inventory(ProductName, Price) VALUES ('banana', 150); INSERT INTO Inventory(ProductName, Price) VALUES ('orange', 154);
-GO 1000
-
-ALTER TABLE Inventory
-ADD PRIMARY KEY (ID);
-GO
-
-CREATE TABLE table_customer(
-    customerID INT IDENTITY(1, 1),
-    customerName NVARCHAR(100),
-    customerAge int CHECK(customerAge > 0)
-);
-GO
-
-INSERT INTO table_customer(customerName,customerAge) VALUES('John', 30); INSERT INTO table_customer(customerName,customerAge) VALUES('John', 30);
-GO 1000
-
-ALTER TABLE table_customer
-ADD PRIMARY KEY (customerID);
-GO
-
-SELECT * FROM Inventory
-GO
-
-SELECT * FROM table_customer
-GO
-
-EXEC sys.sp_cdc_enable_db
-GO
+-- CREATE DATABASE Demo_DATABASE;
+-- GO
+--
+-- USE Demo_DATABASE;
+-- GO
+--
+-- CREATE TABLE Inventory
+-- (
+--     ID          INT IDENTITY (1, 1),
+--     ProductName nvarchar(max),
+--     Price       numeric(18, 0)
+-- );
+-- GO
+--
+-- INSERT INTO Inventory(ProductName, Price) VALUES ('banana', 150); INSERT INTO Inventory(ProductName, Price) VALUES ('orange', 154);
+-- GO 1000
+--
+-- ALTER TABLE Inventory
+-- ADD PRIMARY KEY (ID);
+-- GO
+--
+-- CREATE TABLE table_customer(
+--     customerID INT IDENTITY(1, 1),
+--     customerName NVARCHAR(100),
+--     customerAge int CHECK(customerAge > 0)
+-- );
+-- GO
+--
+-- INSERT INTO table_customer(customerName,customerAge) VALUES('John', 30); INSERT INTO table_customer(customerName,customerAge) VALUES('John', 30);
+-- GO 1000
+--
+-- ALTER TABLE table_customer
+-- ADD PRIMARY KEY (customerID);
+-- GO
+--
+-- SELECT * FROM Inventory
+-- GO
+--
+-- SELECT * FROM table_customer
+-- GO
+--
+-- EXEC sys.sp_cdc_enable_db
+-- GO
