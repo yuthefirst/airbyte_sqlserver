a026038c34794aa269602b5016fe495daae8761e
yuthefirst <khanhduy9cnct@Gmail.com>
2022-08-22 12:53:32 +0700

    Init commit

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM mcr.microsoft.com/mssql/server:2019-latest
+FROM mcr.microsoft.com/mssql/server:2022-latest
 ENV ACCEPT_EULA=Y
 ENV MSSQL_SA_PASSWORD="Pass@123456"
 ENV MSSQL_TCP_PORT=1433
---
+++ dbt_postgres/models/mart/mart_fullmoon_reviews.sql
@@ -0,0 +1,26 @@
+{{ config(
+    materialized = 'table',
+) }}
+
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
+WITH fct_reviews AS (
+    SELECT * FROM {{ ref('fct_reviews') }}
+),
+
+full_moon_dates AS (
+    SELECT * FROM {{ ref('seed_full_moon_dates') }}
+)
+
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
+SELECT
+    r.*,
+    CASE
+    WHEN fm.full_moon_date IS NULL THEN 'not full moon'
+    ELSE 'full moon'
+    END AS is_full_moon
+FROM
+    fct_reviews
+    r
+    LEFT JOIN full_moon_dates
+    fm
+    ON (TO_DATE(r.review_date, 'YYYY-MM-DD') = INTERVAL '1 DAY' + fm.full_moon_date)
